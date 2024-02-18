5b046c715a62a900373d6afaf3ab3aa62ad360c2
gauravc94 <gcserves86@gmail.com>
2024-02-19 01:31:00 +0530

    First Commit. Transformed html to JSX except the class to className, and created the basic components completing the app layout in the process.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <!-- Navigation--> */}
+      <NavBar />
+
+      {/* <!-- Header--> */}
+      <Header />
+
+      {/* <!-- Section--> */}
+      <Section />
+
+      {/* <!-- Footer--> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Footer component with copyright information
+export default function Footer() {
+    return (
+    <footer class="py-5 bg-dark">
+        {/* Copyright text*/}
+        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+    </footer>
+    )
+}
